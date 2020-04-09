5abd6d5edc6dc3d0dd73a747212a48b11edc1234
= <=>
2020-04-09 16:22:04 +0100

    Fixed Config Param

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask
 app = Flask(__name__)
 
 #app.config.from_object(os.environ['APP_SETTINGS'])
-cfg = os.environ['APP_SETTINGS'][1:-1]
+cfg = os.environ['APP_SETTINGS'].replace('"','')
 print(cfg)
 app.config.from_object(cfg)
 
